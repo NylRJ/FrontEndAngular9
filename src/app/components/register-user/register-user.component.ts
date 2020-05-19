1aef4e2d1e0f8581726731711ee0526a149842ba
Moises <loveloungeoficial@gmail.com>
2020-05-19 02:49:42 -0300

    Register implemented

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDTO } from 'src/app/core/model/userDTO';
+import { ApiService } from 'src/app/core/api.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-register-user',
@@ -6,10 +9,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-user.component.scss']
 })
 export class RegisterUserComponent implements OnInit {
-
-  constructor() { }
+  public user = new UserDTO;
+  constructor(private apiServe:ApiService, private location:Location) { }
 
   ngOnInit() {
+  }
+
+  save():void{
+      this.apiServe.registerUser(this.user).subscribe(data =>{
+        console.log('Usuario registrado com sucesso!');
+      }, error =>{
+        console.log('Erro ao registrado Usuario ', error);
+      });
+  }
+  goback(){
+    this.location.back();
   }
 
 }
